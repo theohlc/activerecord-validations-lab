db08441076e548bd1a62c24bdd18f0cc67ffef79
theo <teddyhoover@gmail.com>
2021-02-23 10:39:38 -0600

    commit tree 9bb137a81d37c4831628b804f5d4005650136f9e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    validate :clickbait
+
+    CLICKBAIT = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top [0-9]*/i,
+        /Guess/i
+    ]
+
+    def clickbait
+        if CLICKBAIT.none? { |pat| pat.match title }
+            errors.add(:title, "title not clickbaity")
+        end
+    end
 end
